7568e6a037457fd8a353a923814fe078f55f212e
Nam Kim <nryulkim@gmail.com>
2017-01-16 17:47:39 -0500

    Add brand api

---
+++ frontend/util/brand_api.js
@@ -0,0 +1,43 @@
+export const getAllBrands = (success, error) => {
+  $.ajax({
+    method: "GET",
+    url: "/api/brands",
+    success,
+    error
+  });
+};
+
+export const newBrand = (brand, success, error) => {
+  $.ajax({
+    method: "POST",
+    url: "/api/brands",
+    data: brand,
+    dataType: "json",
+    contentType: false,
+    processData: false,
+    success,
+    error
+  });
+};
+
+export const updateBrand = (brand, success, error) => {
+  $.ajax({
+    method: "PATCH",
+    url: `/api/brands/${brand.id}`,
+    data: brand,
+    dataType: "json",
+    contentType: false,
+    processData: false,
+    success,
+    error
+  });
+};
+
+export const destroyBrand = (id, success, error) => {
+  $.ajax({
+    method: "DELETE",
+    url: `/api/brands/${id}`,
+    success,
+    error
+  });
+};
