01e8f756df648b07b45f2c6f046948499b20c02c
Nam Kim <nryulkim@gmail.com>
2017-01-16 17:47:39 -0500

    Add brandsAPI to window for testing

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from './store/store.js';
-import Root from './components/root.jsx';
+import configureStore from './store/store';
+import Root from './components/root';
+import * as BrandApi from './util/brand_api';
 
+window.BrandAPI = BrandApi;
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
