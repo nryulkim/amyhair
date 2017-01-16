56151d3c4a1cd58221e28eaf06cfc6d8120f28dc
Nam Kim <nryulkim@gmail.com>
2017-01-16 17:52:17 -0500

    Create login

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create({ username: 'admin', password: 'amyhairpass123'})
+
 
 COLORS = {
   "solid":[
