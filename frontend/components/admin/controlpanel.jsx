65eb2b61b930f1521335c6d4043d249c2033a537
Nam Kim <nryulkim@gmail.com>
2017-01-16 17:47:41 -0500

    Add control panel menu

--- frontend/components/admin/controlpanel.jsx
+++ frontend/components/admin/controlpanel.jsx
@@ -5,15 +5,37 @@ class CPanel extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      type: 'addproduct'
     };
 
     this.handleLogOut = this.handleLogOut.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.getForm = this.getForm.bind(this);
   }
 
   handleLogOut(){
     this.props.logout();
     const router = this.props.router;
     window.setTimeout(() => router.push('/login'), 200);
+  }
+
+  handleChange(type){
+    return () => this.setState({ type: type });
+  }
+
+  getForm(){
+    const { type } = this.state;
+    switch(type){
+      case "addproduct":
+        return (<h1>Add product form goes here</h1>);
+      case "removeproduct":
+        return (<h1>Method to remove product</h1>);
+      case "addcolor":
+        return (<h1>Some way to add colors</h1>);
+      case "removecolor":
+        return (<h1>Gotta remove colors too</h1>);
+    }
+    return null;
   }
 
   render(){
@@ -25,8 +47,22 @@ class CPanel extends React.Component {
           <h2>Control Panel</h2>
         </header>
         <div className="control-panel">
-          <button onClick={this.handleLogOut}>Log Out</button>
-          <h1> Things will go here </h1>
+          <div className="clrfx">
+            <button onClick={this.handleLogOut}>Log Out</button>
+          </div>
+          <div className="control-main">
+            <div className="control-list">
+              <ul>
+                <li><a onClick={this.handleChange("addproduct")}>Add Product</a></li>
+                <li><a onClick={this.handleChange("removeproduct")}>Remove Product</a></li>
+                <li><a onClick={this.handleChange("addcolor")}>Add Color</a></li>
+                <li><a onClick={this.handleChange("removecolor")}>Remove Color</a></li>
+              </ul>
+            </div>
+            <div className="control-content">
+              {this.getForm()}
+            </div>
+          </div>
         </div>
       </article>
     );
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -12,6 +12,9 @@ const _default = {
       }
     }
   }
+  colors: {
+    
+  }
 }
 
 export default (state = {}) => {
